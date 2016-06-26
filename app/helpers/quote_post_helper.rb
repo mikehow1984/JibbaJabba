dbefba50c7076141d282fb571e95076a4be3bab6
Michael Howard <mikehow1005@gmail.com>
2016-06-25 22:32:18 -0400

    added quoting and post linking

--- app/helpers/quote_post_helper.rb
+++ app/helpers/quote_post_helper.rb
@@ -2,7 +2,8 @@ module QuotePostHelper
 	def post_linkifier(text)
 		text.gsub!(/(@\d+)/, "<a href='#\\1'>\\1</a>")
 		text.gsub!(/<a href='#@(\d+)'>@(\d+)<\/a>/, "<a href='#\\1'>@\\1</a>")
-		comment_arr = text.split(/(<a href='#\d+'>@\d+<\/a>)\s*/)
+		text.gsub!(/\*\*(.+)\*\*/, "<div class='quote'>\\1</div>")
+		comment_arr = text.split(/(<a href='#\d+'>@\d+<\/a>|<div class='quote'>|<\/div>)\s*/)
 		comment_arr
 	end
 
