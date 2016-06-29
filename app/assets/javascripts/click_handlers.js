bcc242dbfc071d4b2057561c66566fa9170d40de
Michael Howard <mikehow1005@gmail.com>
2016-06-29 12:51:43 -0400

    added more styling/completing index page

--- app/assets/javascripts/click_handlers.js
+++ app/assets/javascripts/click_handlers.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
 	$(".post-id").click(function(){
 		var postID = $(this).attr('id').substring(5);
-		var quotePost = $('#post-content-form').val() + "@" + postID + " **" + $("#post-text-" + postID).text().trim() + "**\n";
+		var postQuoted = $("#post-text-" + postID).text();
+		postQuoted = postQuoted.replace(/\*{2,}(.|\s)+\*{2,}/, "").trim();
+		var quotePost = $('#post-content-form').val() + "@" + postID + " **" + postQuoted + "**\n";
 		$("#post-content-form").val($("#post-content-form").val() + quotePost);
 	});
 });
