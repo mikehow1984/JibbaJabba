02a4adab6526805f658bc27791ad91c36b95becd
Michael Howard <mikehow1005@gmail.com>
2016-07-23 14:59:16 -0400

    Added partial for posts

--- app/helpers/quote_post_helper.rb
+++ app/helpers/quote_post_helper.rb
@@ -1,9 +1,9 @@
 module QuotePostHelper
 	def post_linkifier(text)
-		text.gsub!(/(@\d+)/, "<a href='#\\1'>\\1</a>")
-		text.gsub!(/<a href='#@(\d+)'>@(\d+)<\/a>/, "<a href='#\\1'>@\\1</a>")
+		text.gsub!(/(@(\d+|TopicStarter))/, "<a href='#\\1'>\\1</a>")
+		text.gsub!(/<a href='#@(\d+|TopicStarter)'>@(\d+|TopicStarter)<\/a>/, "<a href='#\\1'>@\\1</a>")
 		text.gsub!(/\*\*(.+)\*\*/, "<div class='quote'>\\1</div>")
-		comment_arr = text.split(/(<a href='#\d+'>@\d+<\/a>|<div class='quote'>|<\/div>)\s*/)
+		comment_arr = text.split(/(<a href='#\d+'>@\d+<\/a>|<a href='#TopicStarter'>@TopicStarter<\/a>|<div class='quote'>|<\/div>)\s*/)
 		comment_arr
 	end
 
