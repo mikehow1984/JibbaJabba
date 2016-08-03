cb0ff8bd431fbe0becac8a971ebe0a10935a47fa
Michael Howard <mikehow1005@gmail.com>
2016-08-03 15:22:26 -0400

    hopefully fixed dependency problems

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,6 @@
 class Topic < ActiveRecord::Base
+	include Cloudsearchable
+	
 	has_many :posts, :dependent => :destroy
 	accepts_nested_attributes_for :posts
 
@@ -8,10 +10,13 @@ class Topic < ActiveRecord::Base
 	validates :title, :length => { :minimum => 3, :maximum => 50, :too_short => "Titles must be at least %{count} characters long", :too_Long => "Titles cannot be over %{count} characters long" }, :presence => true
 	
 	validates :content, :length => { :minimum => 5, :maximum => 500, :too_short => "Content must be at least %{count} characters long", :too_Long => "Content cannot be over %{count} characters long" }, :presence => true
-	
-	searchable do 
-		text (:title)
-		text (:content)
+
+	#Cloudsearchable search
+	attr_accessor :title, :content, :posts, :unix_time, :id
+	index_in_cloudsearch do |idx|
+		text :topic_title, :result_enabled => true, :search_enabled => true, :source => :title			
+		text :topic_content, :result_enabled => true, :search_enabled => true, :source => :content
+		#how to make this work for cloudsearchable
 		text (:posts) do 
 			posts.map(&:content).compact.join(" ")
 		end
