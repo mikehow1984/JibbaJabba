91926c4e7f68dc0372f2b96ddc591828e04172c6
Michael Howard <mikehow1005@gmail.com>
2016-08-08 15:37:27 -0400

    development cleanup complete

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,7 @@
 class Topic < ActiveRecord::Base
-	include Cloudsearchable
-	
+	#include Cloudsearchable
+	#implement cloudsearchable when development is working
+
 	has_many :posts, :dependent => :destroy
 	accepts_nested_attributes_for :posts
 
@@ -11,22 +12,36 @@ class Topic < ActiveRecord::Base
 	
 	validates :content, :length => { :minimum => 5, :maximum => 500, :too_short => "Content must be at least %{count} characters long", :too_Long => "Content cannot be over %{count} characters long" }, :presence => true
 
-	#Cloudsearchable search
-	attr_accessor :title, :content, :posts, :unix_time, :id
-	index_in_cloudsearch do |idx|
-		text :topic_title, :result_enabled => true, :search_enabled => true, :source => :title			
-		text :topic_content, :result_enabled => true, :search_enabled => true, :source => :content
-		#how to make this work for cloudsearchable
-		text (:posts) do 
+	searchable do
+		text (:title)
+		text (:content)
+		text (:posts) do
 			posts.map(&:content).compact.join(" ")
 		end
 
 		integer (:unix_time)
 		integer (:id)
 		string :sort_title do
-			title.downcase.gsub(/^(an?|the)/, '')
+			title.downcase.gsub(/^(an?|the)?/, '')
 		end
-	end	
+	end
+	#Cloudsearchable search
+	#attr_accessor :title, :content, :posts, :unix_time, :id
+	
+	#index_in_cloudsearch do |idx|
+		#text :topic_title, :result_enabled => true, :search_enabled => true, :source => :title			
+		#text :topic_content, :result_enabled => true, :search_enabled => true, :source => :content
+		#how to make this work for cloudsearchable
+		#text (:posts) do 
+			#posts.map(&:content).compact.join(" ")
+		#end
+
+		#integer (:unix_time)
+		#integer (:id)
+		#string :sort_title do
+			#title.downcase.gsub(/^(an?|the)/, '')
+		#end
+	#end	
 end
 
 
